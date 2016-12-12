8a18383d67b98ec7651da23f9364cd93d94686ae
Avelino <thiago@avelino.xxx>
2016-12-12 04:11:10 -0200

    used image build

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ var DataOut map[string]interface{}
 
 func main() {
 	client := provision.FnClient("")
-	stdout := provision.FnContainer(client, "gofn-python")
+	imageName, _ := provision.FnImageBuild(client, "testDocker", "Dockerfile", "python")
+	stdout := provision.FnContainer(client, imageName)
 
 	if err := json.Unmarshal([]byte(stdout.String()), &DataOut); err != nil {
 		panic(err)
