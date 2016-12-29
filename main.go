a2c71ccd50d0848720d67e52a2fe3c244b7014d5
Cesar Gimenes <crg@crg.eti.br>
2016-12-29 17:41:58 -0200

    POC... only creates an image if it does not exist.

--- main.go
+++ main.go
@@ -3,14 +3,40 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 
+	docker "github.com/fsouza/go-dockerclient"
 	"github.com/nuveo/gofn/provision"
 )
 
 func main() {
 	client := provision.FnClient("")
-	imageName, _ := provision.FnImageBuild(client, "testDocker", "Dockerfile", "python")
-	stdout := provision.FnContainer(client, imageName)
+
+	img, err := provision.FnFindImage(client, "python")
+	if err != nil {
+		log.Println(err)
+	}
+
+	var imageName string
+	var container *docker.Container
+
+	if img.ID == "" {
+		imageName, _ = provision.FnImageBuild(client, "testDocker", "Dockerfile", "python")
+	} else {
+		imageName = "gofn/" + "python"
+	}
+
+	container, err = provision.FnContainer(client, imageName)
+	if err != nil {
+		panic(err)
+	}
+
+	stdout := provision.FnRun(client, container.ID)
+
+	provision.FnRemove(client, container.ID)
+	if err != nil {
+		panic(err)
+	}
 
 	if err := json.Unmarshal([]byte(stdout.String()), &provision.DataOut); err != nil {
 		panic(err)
