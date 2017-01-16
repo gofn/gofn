e519edd4cb8d4f2ad92428d36e7fd18f530f8429
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-01-16 17:34:43 -0200

    define machine attributes

--- iaas/iaas.go
+++ iaas/iaas.go
@@ -10,4 +10,11 @@ type Iaas interface {
 }
 
 // Machine defines a generic machine
-type Machine struct{}
+type Machine struct {
+	ID     string `json:"id"`
+	Status string `json:"status"`
+	IP     string `json:"ip"`
+	Image  string `json:"image"`
+	Name   string `json:"name"`
+	Kind   string `json:"kind"`
+}
