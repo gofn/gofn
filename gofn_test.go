efaa21197dda425906451c232049fb1a83242035
Cesar Gimenes <crg@crg.eti.br>
2017-03-08 11:09:18 -0300

    Corrects panic if path to the Dockerfile does not exist (#55)
    * Fix panic that occurs when Dockerfile is not found
    
    * Run must return error if the path to Dockerfile does not exist
    
    * being more verbose

---
+++ gofn_test.go
@@ -0,0 +1,20 @@
+package gofn
+
+import (
+	"testing"
+
+	"github.com/nuveo/gofn/provision"
+)
+
+func TestRun(t *testing.T) {
+
+	buildOpts := &provision.BuildOptions{
+		ContextDir: "./error_path", // this path must not exist
+		ImageName:  "testgofn",
+	}
+	_, _, err := Run(buildOpts, nil)
+	if err == nil {
+		t.Fatal("Expected error but returned nil, this test must fail because the path to Dockerfile does not exist")
+	}
+
+}
