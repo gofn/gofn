54be79ce45341e942f9cd5a08f212a65173f239e
cassiobotaro <cassiobotaro@gmail.com>
2018-07-04 04:14:22 -0300

    refactor digital ocean structure and write tests

--- iaas/digitalocean/digitalocean.go
+++ iaas/digitalocean/digitalocean.go
@@ -2,10 +2,8 @@ package digitalocean
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strconv"
 
 	"context"
@@ -17,14 +15,8 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-const (
-	defaultRegion    = "nyc3"
-	defaultSize      = "1gb"
-	defaultImageSlug = "ubuntu-16-04-x64"
-)
-
-// Digitalocean definition, represents a concrete implementation of an iaas
-type Digitalocean struct {
+// provider definition, represents a concrete implementation of an iaas
+type provider struct {
 	Client            libmachine.API
 	Host              *host.Host
 	Name              string
@@ -48,30 +40,6 @@ type driverConfig struct {
 	} `json:"Driver"`
 }
 
-// GetRegion returns region or default if empty
-func (do Digitalocean) GetRegion() string {
-	if do.Region == "" {
-		return defaultRegion
-	}
-	return do.Region
-}
-
-// GetSize returns size or default if empty
-func (do Digitalocean) GetSize() string {
-	if do.Size == "" {
-		return defaultSize
-	}
-	return do.Size
-}
-
-// GetImageSlug returns image slug  or default if empty
-func (do Digitalocean) GetImageSlug() string {
-	if do.ImageSlug == "" {
-		return defaultImageSlug
-	}
-	return do.ImageSlug
-}
-
 func getConfig(machineDir, hostName string) (config *driverConfig, err error) {
 	configPath := fmt.Sprintf("%s/%s/config.json", machineDir, hostName)
 	raw, err := ioutil.ReadFile(configPath)
@@ -85,47 +53,38 @@ func getConfig(machineDir, hostName string) (config *driverConfig, err error) {
 	return
 }
 
-func New() (do *Digitalocean, err error) {
+func New(token string) (do *provider, err error) {
 	var uid uuid.UUID
 	uid, err = uuid.NewV4()
 	if err != nil {
 		return
 	}
-
 	name := fmt.Sprintf("gofn-%s", uid.String())
 	clientPath := "/tmp/" + name
 	c := libmachine.NewClient(clientPath, clientPath+"/certs")
-
-	do = &Digitalocean{
+	driver := digitalocean.NewDriver(name, clientPath)
+	driver.AccessToken = token
+	do = &provider{
 		Client:     c,
 		Name:       name,
 		ClientPath: clientPath,
 	}
-	return
-}
-
-// CreateMachine on digitalocean
-func (do *Digitalocean) CreateMachine() (machine *iaas.Machine, err error) {
-	driver := digitalocean.NewDriver(do.Name, do.ClientPath)
-	key := os.Getenv("DIGITALOCEAN_API_KEY")
-	if key == "" {
-		err = errors.New("You must provide a Digital Ocean API Key")
-		return
-	}
-	driver.AccessToken = key
-	driver.Size = do.GetSize()
-	driver.Region = do.GetRegion()
-	driver.Image = do.GetImageSlug()
 
 	data, err := json.Marshal(driver)
 	if err != nil {
 		return
 	}
 
-	do.Host, err = do.Client.NewHost("digitalocean", data)
+	do.Host, err = do.Client.NewHost(driver.DriverName(), data)
 	if err != nil {
 		return
 	}
+
+	return
+}
+
+// CreateMachine on digitalocean
+func (do *provider) CreateMachine() (machine *iaas.Machine, err error) {
 
 	err = do.Client.Create(do.Host)
 	if err != nil {
@@ -149,7 +108,7 @@ func (do *Digitalocean) CreateMachine() (machine *iaas.Machine, err error) {
 }
 
 // DeleteMachine Shutdown and Delete a droplet
-func (do *Digitalocean) DeleteMachine(machine *iaas.Machine) (err error) {
+func (do *provider) DeleteMachine(machine *iaas.Machine) (err error) {
 	err = do.Host.Driver.Remove()
 	defer do.Client.Close()
 	if err != nil {
@@ -159,7 +118,7 @@ func (do *Digitalocean) DeleteMachine(machine *iaas.Machine) (err error) {
 }
 
 // ExecCommand on droplet
-func (do *Digitalocean) ExecCommand(machine *iaas.Machine, cmd string) (output []byte, err error) {
+func (do *provider) ExecCommand(machine *iaas.Machine, cmd string) (output []byte, err error) {
 	out, err := do.Host.RunSSHCommand(cmd)
 	if err != nil {
 		return
