ef80f7a865dd92c7eaf82d51cc11e156976e5d81
felipeweb <felipeweb.programador@gmail.com>
2018-12-11 03:15:42 -0200

    fix build remote

--- provision/docker.go
+++ provision/docker.go
@@ -97,9 +97,6 @@ func FnImageBuild(client *docker.Client, opts *BuildOptions) (Name string, Stdou
 	if opts.Dockerfile == "" {
 		opts.Dockerfile = "Dockerfile"
 	}
-	if opts.ContextDir == "" {
-		opts.ContextDir = "./"
-	}
 	if opts.Auth.ServerAddress == "" {
 		opts.Auth.ServerAddress = "https://index.docker.io/v1/"
 	}
@@ -113,7 +110,6 @@ func FnImageBuild(client *docker.Client, opts *BuildOptions) (Name string, Stdou
 			return
 		}
 		opts.Auth.IdentityToken = status.IdentityToken
-		fmt.Println(">>>>>>>>>>>>>>>", opts.Auth, status)
 	}
 	stdout := new(bytes.Buffer)
 	Name = opts.GetImageName()
